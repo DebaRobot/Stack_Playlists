b4be231682bfd531981439758dbb6e796e87e798
DebaRobot <71641088+DebaRobot@users.noreply.github.com>
2022-07-14 04:26:06 +0530

    Update Insert Element at the Bottom.cpp

--- Insert Element at the Bottom.cpp
+++ Insert Element at the Bottom.cpp
@@ -25,25 +25,3 @@ stack<int> pushAtBottom(stack<int>& myStack, int x)
     return myStack;
 }
 
-
-    int main()
-{
-    // Input
-    stack<int> S;
-    S.push(5);
-    S.push(4);
-    S.push(3);
-    S.push(2);
-    S.push(1);
-
-    int N = 7;
-    pushAtBottom(S, N);
-     while (!S.empty())
-    {
-        int p=S.top();
-        S.pop();
-        cout << p << " ";
-    }
-    return 0;
-
-}
